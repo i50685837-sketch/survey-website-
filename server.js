2a0993da735a9cecd46c455ef1a7e02f2f1c1345
i50685837-sketch <i50685837@gmail.com>
2026-07-22 19:00:38 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+// Connect database
+connectDB();
+
+
+// Routes
+app.use("/api/auth", require("./routes/auth"));
+
+
+// Test API
+app.get("/", (req,res)=>{
+    res.json({
+        message:"Survey Hub API Running 🚀"
+    });
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+});
